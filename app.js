d4c677bbd38514c5181199bd3b585ed1aafdc9ff
jebi2420 <jebi2420@gmail.com>
2024-05-04 19:31:04 +0900

    [add] mongoose test

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/mongoose-test');
+const validator = require('validator');
 
 const { Schema } = mongoose;
 
@@ -13,27 +14,33 @@ const userSchema = new Schema({
         required:true,
         validate:{
             validator: function(value){
-                if(!value.includes("@")) throw new Error("This is not an email")
+                if(!validator.isEmail(value))
+                throw new Error("This is not an email")
             }
         }
     },
     password:{
         type:String,
-        required:true
+        required:true,
+        trim: true
     },
     age:{
-        type:Number
+        type:Number,
+        default: 0
     },
 });
 
 // Schema를 토대로 model을 만들 수 있다
 const User = mongoose.model("User", userSchema); // "User"라는 model 생성
 
-const newUser = new User ({
-    name: "Dora",
-    email: "qwe11gamil.com",
-    password: "4445"
-});
+// const newUser = new User ({
+//     name: "김철수",
+//     email: "www9@gamil.com",
+//     password: "1122"
+// });
 
-newUser.save().then((value) => console.log("value is", value));
+// newUser.save().then((value) => console.log("value is", value));
 
+User.find({ name: "김철수"})
+    .select("name -_id")
+    .then(value => console.log("all data : ", value));
